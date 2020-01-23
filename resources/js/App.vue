86f4030551bec20c727c0df478cea73d49e60ca2
erandadava <erandadava@gmail.com>
2020-01-23 14:23:28 +0700

    laravel with vue js crud task

---
+++ resources/js/App.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="container">
+        <div class="container">
+        	<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+				<ul class="navbar-nav">
+					<li class="nav-item">
+						<router-link to="/" class="nav-link"> Home </router-link>
+					</li>
+					<li>
+						<router-link to="/create" class="nav-link"> Create Posts </router-link>
+					</li>
+					<li>
+						<router-link to="/posts" class="nav-link"> Posts </router-link>
+					</li>
+				</ul>
+        	</nav> <br />
+<!--        <transition name="fade">
+                <router-view></router-view>
+            </transition> -->
+			<transition>
+                <router-view></router-view>
+            </transition>
+        </div>
+    </div>
+</template>
+
+<style>
+    .fade-enter-active, .fade-leave-active {
+      transition: opacity .5s
+    }
+    .fade-enter, .fade-leave-active {
+      opacity: 0
+    }
+</style>
+
+<script>
+
+    export default{
+    }
+ </script>
---
+++ resources/js/components/HomeComponent.vue
@@ -0,0 +1,18 @@
+<template>
+	<div class="row justify-content-center">
+		<div class="col-md-8">
+			<div class="card card-default">
+				<div class="card-header"> Home Component </div>
+				<div class="card-body">i'm the Home Component.</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		mounted(){
+			console.log('component mounted. ')
+		}
+	}
+</script>
